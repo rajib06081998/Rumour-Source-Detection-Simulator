c4340630fafd0f1d02902ffc472bf234c67f8cbc
rajib06081998 <rajeevsaha853@gmail.com>
2020-06-21 16:09:17 +0530

    first commit

---
+++ adjacencyList.mjs
@@ -0,0 +1,12 @@
+export const adjacencyList=function(links,nodesCount){
+	const v=[];
+	for(let i=0;i<nodesCount;i++)
+		v.push([]);
+
+	for(let i=0;i<links.length;i++){
+		// console.log(links[i].source,links[i].destination);
+	    v[links[i].source.value].push(links[i].destination.value);
+	    v[links[i].destination.value].push(links[i].source.value);
+	}
+	return v;
+}
